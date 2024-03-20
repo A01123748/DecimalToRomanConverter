04e760c635d37b49b83992b95cad83fcd700d049
Eliseo Fuentes <efuentes@eliseos-mbp.lan>
2024-03-20 16:28:10 -0600

    complete exercise

--- MyClass.java
+++ MyClass.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 /* Integer to Roman number between 1 - 3000
 I -> III
 IV
@@ -26,64 +25,57 @@ MCCCXLIII
 */
 
 class MyClass {
-
-    private static HashMap<Integer, String> map;
-    static {
-        map = new HashMap(Integer, String);
-        map.put(0, "I");
-        map.put(1, "V");
-
-    }
-    private static HashMap<Integer, String> map2;
-    static {
-        map2 = new HashMap(Integer, String);
-        map2.put(0, "X");
-        map2.put(1, "L");
-    }
-    private static HashMap<Integer, String> map3;
-    static {
-        map3 = new HashMap(Integer, String);
-        map3.put(0, "C");
-        map3.put(1, "D");
-    }
-
-    private static HashMap<Integer, String> map4;
-    static {
-        map4 = new HashMap(Integer, String);
-        map4.put(0, "M");
-        map4.put(1, "P");
-    }
-
-    private HashMap<Integer, String>[] charactersArray = { map, map2, map3, map4 };
-
     public static void main(String[] args) {
-        System.out.println(convertNumber(5));
+        for (int i = 1; i < 3549; i++)
+            System.out.println(convertNumber(i));
     }
 
-    public static String convertNumber(int n) {
-        int number = n;
-        String result = "";
+    public static String convertNumber(int number) {
+        StringBuilder result = new StringBuilder();
         int level = 0;
+
         while (number > 0) {
-            int residue = n % 10;
-            number = n / 10;
-            result += translateToRoman(residue, level++);
+            int residue = number % 10;
+            number = number / 10;
+            result.insert(0, translateToRoman(residue, level++));
         }
 
-        return result;
+        return result.toString();
     }
 
-    public static String translateToRoman(int residue, int level){
-        map = maps[level];
-        
-       // [map, map, map, map]
-        if(re == 1) return map[0];// "I"
-        map[1]// "V"
-        
-        1 - 3 // I, II, II
-        4 // IV
-        5 // V
-        6 - 8 // VI, VII, VIII
-        9 // IV
+    public static String translateToRoman(int residue, int level) {
+        // Decided to move this here as its not being used elswhere
+        String[] units = { "I", "V" };
+        String[] tens = { "X", "L" };
+        String[] hundreds = { "C", "D" };
+        String[] thousands = { "M", "G" };
+        String[][] charactersArray = { units, tens, hundreds, thousands };
+        String[] currentMap = charactersArray[level];
+        // 9 -> IV
+        if (residue == 9) {
+            return currentMap[0] + "" + charactersArray[level + 1][0];
+            // 5 - 8 -> V, VI, VII, VIII
+        } else if (residue >= 5) {
+            return currentMap[1] + repeat(currentMap[0], residue - 5);
+            // 4 -> IV
+        } else if (residue == 4) {
+            return currentMap[0] + "" + currentMap[1];
+        }
+        // 1 - 3 -> I, II, III
+        else {
+            return repeat(currentMap[0], residue);
+        }
+    }
+
+    public static String repeat(String s, int n) {
+        if (s == null)
+            return null;
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            builder.append(s);
+        }
+        return builder.toString();
     }
 }
